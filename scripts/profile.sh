104a060b8074de3d0ee7e6aa395be92772e4be01
Hei-OZ <cacaotomato@gmail.com>
2021-10-31 11:55:07 +0900

    무중단 배포 코드 작성 - 오타 수정

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
-# 쉬고 있는 profile 찾기: real1이 사용중이면 real2가 쉬고 있고, 반대면 real1이 쉬고 있음
+# bash는 return value가 안되니 *제일 마지막줄에 echo로 해서 결과 출력*후, 클라이언트에서 값을 사용한다
 
+# 쉬고 있는 profile 찾기: real1이 사용중이면 real2가 쉬고 있고, 반대면 real1이 쉬고 있음
 function find_idle_profile()
 {
-  RESONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile) # 현재 url 정상 수행중인지 확인.
+  RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile) # 현재 url 정상 수행중인지 확인.
   # 응답값을 HttpStatus 로 받기에 200이 정상이고 이외 400~503 사이로 에러 발생하면 real2를 현재 profile로 사용
 
-  if [ ${RESONSE_CODE} -ge 400 ]  # 400 보다 크면(즉 40x, 50x 에러 모두 포함)
+  if [ ${RESPONSE_CODE} -ge 400 ]  # 400 보다 크면(즉 40x, 50x 에러 모두 포함)
   then
     CURRENT_PROFILE=real2
   else
-    CURRENT_PROFILE=real1
+    CURRENT_PROFILE=$(curl -s http://localhost/profile)
   fi
 
   if [ ${CURRENT_PROFILE} == real1 ]
