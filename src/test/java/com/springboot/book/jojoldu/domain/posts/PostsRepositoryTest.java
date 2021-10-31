0bb9b071a27fa337fe40afee7c3928bbbd424b13
Hei-OZ <cacaotomato@gmail.com>
2021-10-31 14:23:19 +0900

    SpringBoot 버전 2.4.1로 변경 JUnit 버전 5로 변경

--- src/test/java/com/springboot/book/jojoldu/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/springboot/book/jojoldu/domain/posts/PostsRepositoryTest.java
@@ -1,26 +1,26 @@
 package com.springboot.book.jojoldu.domain.posts;
 
 
-import org.junit.After;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest // 별다른 설정 없이도 H2데이터베이스 자동 실행해주는 어노테이션.
 public class PostsRepositoryTest {
 
     @Autowired
     PostsRepository postsRepository;
 
-    @After  // JUnit 에서 단위 테스트가 끝날때마다 수행 할 메소드를 지정하는 어노테이션.
+    @AfterEach  // JUnit 에서 단위 테스트가 끝날때마다 수행 할 메소드를 지정하는 어노테이션.
     public void cleanup() {
         postsRepository.deleteAll();    // deleteAll() = 테이블 posts의 모든 데이터 삭제 메소드
     }
--- src/test/java/com/springboot/book/jojoldu/web/HelloControllerTest.java
+++ src/test/java/com/springboot/book/jojoldu/web/HelloControllerTest.java
@@ -1,14 +1,14 @@
 package com.springboot.book.jojoldu.web;
 
 import com.springboot.book.jojoldu.config.auth.SecurityConfig;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
@@ -16,7 +16,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
-@RunWith(SpringRunner.class)    // 테스트 진행시 JUnit에 내장된 실행자 외 다른 실행자를 실행시킴. 여기서는 SpringRunner라는 스프링 실행자를 사용. 즉 스프링부트 테스트와 JUnit 사이의 연결자 역할
+@ExtendWith(SpringExtension.class)    // 테스트 진행시 JUnit에 내장된 실행자 외 다른 실행자를 실행시킴. 여기서는 SpringExtension라는 스프링 실행자를 사용. 즉 스프링부트 테스트와 JUnit 사이의 연결자 역할
 @WebMvcTest(controllers = HelloController.class,    // WebMvcTest 는 Component를 스캔하지 않으므로 스프링 시큐리티 설정 SecurityConfig 를 생성하기 위한 CustomOAuth2UserService 읽을 수 없어 에러 발생
     excludeFilters = {  // SecurityConfig.class 스캔하지 않도록 필터링
         @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
